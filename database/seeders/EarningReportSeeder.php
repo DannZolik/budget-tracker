7d4bc760bdd4078bfdfe9ac56144acf14c9ff7d2
Oxymina <bumburs.mikus@gmail.com>
2024-09-24 13:21:39 +0300

    Created filament for Earning/Expense reports. Made database tables for these.

---
+++ database/seeders/EarningReportSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EarningReportSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}

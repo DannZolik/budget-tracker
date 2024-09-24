aeeeba1708a0a1d5039706db418374c9bf762f5f
re-glebsdimitrijevs <glebsdimitrijevs@reatom.com>
2024-09-24 14:20:18 +0300

    almost finished categories

--- resources/views/tables/columns/custom-icon-column.blade.php
+++ resources/views/tables/columns/custom-icon-column.blade.php
@@ -1,3 +1,7 @@
-<div class="w-full mb-2">
-    test
+@php
+    $iconColor = $getIconColor();
+@endphp
+
+<div class="rounded-lg w-full mb-2 flex items-center justify-center" style="height: 180px; background-color: {{ $getBgColor() }}">
+    @svg($getIcon(), ['class' => 'w-24 h-24', 'style' => "fill: $iconColor;"])
 </div>

f8d6d7ec96ed671861a73069ff0c930b709e6286
Oxymina <bumburs.mikus@gmail.com>
2024-09-24 14:17:18 +0300

    Began work on a report command function

---
+++ app/Console/Commands/reports.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class reports extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reports';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create reports for expenses and earnings';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('TEST FOR REPORT FUNCTION');
+        return 0;
+    }
+}

13c9e5c11e7c88ef65b1c579d6493e8cbccbd407
21DP1AReim <11DAReimanis@rvt.lv>
2024-09-26 22:59:06 +0300

    Tables and forms for earnings and expenses

---
+++ app/Filament/Resources/EarningsResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EarningsResource\Pages;
+use App\Filament\Resources\EarningsResource\RelationManagers;
+use App\Models\Earnings;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+
+class EarningsResource extends Resource
+{
+    protected static ?string $model = Earnings::class;
+
+    protected static ?string $navigationIcon = 'tabler-coins';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('description'),
+                TextInput::make('category_id'),
+                TextInput::make('sum')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('description'),
+                TextColumn::make('category_id'),
+                TextColumn::make('sum'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEarnings::route('/'),
+            'create' => Pages\CreateEarnings::route('/create'),
+            'edit' => Pages\EditEarnings::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/ExpensesResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ExpensesResource\Pages;
+use App\Filament\Resources\ExpensesResource\RelationManagers;
+use App\Models\Expenses;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+
+class ExpensesResource extends Resource
+{
+    protected static ?string $model = Expenses::class;
+
+    protected static ?string $navigationIcon = 'tabler-brand-shopee';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('description'),
+                TextInput::make('category_id'),
+                TextInput::make('sum')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('description'),
+                TextColumn::make('category_id'),
+                TextColumn::make('sum'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListExpenses::route('/'),
+            'create' => Pages\CreateExpenses::route('/create'),
+            'edit' => Pages\EditExpenses::route('/{record}/edit'),
+        ];
+    }
+}

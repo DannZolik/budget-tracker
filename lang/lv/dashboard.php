d39ef261ab07c8cf266f0bcfaaacb15fdbaf66c4
21DP1AReim <11DAReimanis@rvt.lv>
2024-10-21 19:48:03 +0300

    Translations for dashboard

---
+++ lang/lv/dashboard.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'add_expense' => 'Pievienot izdevumu',
+    'add_earning' => 'Pievienot ienākumu',
+    'earned' => 'Naudas summa, ko esi nopelnījis',
+    'spent' => 'Naudas summa, ko esi iztērējis',
+    'difference' => 'Atšķirība starp taviem ienākumiem un izdevumiem',
+];

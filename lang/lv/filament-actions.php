ebb880a92ced2d3fa682905d442c425446e5a016
Oxymina <96707406+Oxymina@users.noreply.github.com>
2024-11-08 08:28:27 +0200

    Create filament-actions.php

---
+++ lang/lv/filament-actions.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'export' => [
+        'modal' => [
+            'heading' => 'Eksporta iespējas',
+            'form' => [
+                'columns' => [
+                    'label' => 'Kolonnas',
+                ],
+            ],
+            'actions' => [
+                'export' => [
+                    'label' => 'Eksportēt',
+                ],
+            ],
+        ],
+    ],
+];
